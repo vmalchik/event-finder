7019bd6c5bcdd1d253afa8b439fadeb650e32fcc
Victor Malchikov <victor.malchikov@netwitness.com>
2024-05-26 16:49:58 -0700

    style header nav links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,42 @@
+"use client";
+
 import Link from "next/link";
 import Logo from "./Logo";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
+import { motion } from "framer-motion";
 
 const routes = [
   { name: "Home", path: "/" },
-  { name: "Events", path: "/events" },
+  { name: "All Events", path: "/events/all" },
 ];
 
 export default function Header() {
+  const activePathname = usePathname();
   return (
     // mobile first design; specify mobile classes first (e.g. px-3 then sm:px-9)
     <header className="flex justify-between items-center border-b border-white/10 h-14 px-3 sm:px-9">
       <Logo />
-      <nav>
-        <ul className="flex gap-x-6 text-sm">
+      <nav className="h-full">
+        <ul className="h-full flex gap-x-6 text-sm">
           {routes.map((route) => (
             <li
               key={route.path}
-              className="text-white/50 hover:text-white transition"
+              className={clsx(
+                "relative flex items-center hover:text-white transition",
+                {
+                  "text-white": activePathname === route.path,
+                  "text-white/50": activePathname !== route.path,
+                }
+              )}
             >
               <Link href={route.path}>{route.name}</Link>
+              {activePathname === route.path && (
+                <motion.div
+                  layoutId="header-active-link"
+                  className="absolute bg-accent h-1 w-full bottom-0"
+                ></motion.div>
+              )}
             </li>
           ))}
         </ul>
