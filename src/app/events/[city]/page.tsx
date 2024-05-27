deb4cfc0c9be6c6ca0a6b67e535e94a475e4aa23
Victor Malchikov <victor.malchikov@netwitness.com>
2024-05-27 15:05:33 -0700

    add scrollbar to each page to prevent layout shift

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -2,7 +2,7 @@ import H1 from "@/components/H1";
 
 export default function Events() {
   return (
-    <main>
+    <main className="flex flex-col items-center py-24 px-[20px]">
       <H1>Events in Austin</H1>
     </main>
   );
