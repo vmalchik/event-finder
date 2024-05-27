0117271d8531fad9278259b05ca4e98845437197
Victor Malchikov <victor.malchikov@netwitness.com>
2024-05-27 14:54:44 -0700

    enable logo nav to home page

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,14 +1,17 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Logo() {
   return (
-    <Image
-      src="https://bytegrad.com/course-assets/react-nextjs/evento.png"
-      // reserve space for the image to prevent layout content shift
-      // if image is loaded from public directory there is no need to specify width and height or next.config.js
-      width={53}
-      height={12}
-      alt="Event Finder logo"
-    />
+    <Link href="/">
+      <Image
+        src="https://bytegrad.com/course-assets/react-nextjs/evento.png"
+        // reserve space for the image to prevent layout content shift
+        // if image is loaded from public directory there is no need to specify width and height or next.config.js
+        width={53}
+        height={12}
+        alt="Event Finder logo"
+      />
+    </Link>
   );
 }
