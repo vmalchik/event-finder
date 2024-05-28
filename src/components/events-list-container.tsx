97020eccf31a22fe0634ecb0c179696d4b70afb3
Victor Malchikov <victor.malchikov@netwitness.com>
2024-05-27 22:56:39 -0700

    fetch caching notes

--- src/components/events-list-container.tsx
+++ src/components/events-list-container.tsx
@@ -6,6 +6,8 @@ type EventListContainerProps = {
   city: string;
 };
 
+// const MAX_REVALIDATION_WAIT_TIME = 300; // 5 minutes
+
 export default async function EventListContainer({
   city,
 }: EventListContainerProps) {
@@ -18,6 +20,12 @@ export default async function EventListContainer({
   // - data is not bundled with the client improving performance
   const response = await fetch(
     `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
+    // {
+    //   next: {
+    //     revalidate: MAX_REVALIDATION_WAIT_TIME, // NextJS feature to revalidate the data every 5 minutes
+    //   },
+    //   // cache: "no-cache", // NextJS feature to disable server side caching for this request
+    // }
   );
   const events: EventoEvent[] = await response.json();
 
