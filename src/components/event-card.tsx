3bb19f840a8af706632ec0c50d12416f6cf9d89b
Victor Malchikov <victor.malchikov@netwitness.com>
2024-05-27 18:15:00 -0700

    make event card interactive

--- src/components/event-card.tsx
+++ src/components/event-card.tsx
@@ -1,5 +1,6 @@
 import { EventoEvent } from "@/lib/types";
 import Image from "next/image";
+import Link from "next/link";
 
 type EventCardProps = {
   event: EventoEvent;
@@ -30,27 +31,32 @@ export default function EventCard({ event }: EventCardProps) {
     // rounded-xl for rounded corners and use overflow-hidden to prevent image from not having rounded corners at top of the card
     // flex-1 to make card take 1 portion of the available space but allowed to grow to max-width of 500px
     // basis will set minimum width of the card
-    <section className="relative flex flex-col flex-1 basis-80 h-[380px] max-w-[500px] bg-white/[3%] rounded-xl overflow-hidden ">
-      {/* height and width used to prevent layout shift */}
-      <Image
-        src={event.imageUrl}
-        alt={event.name}
-        height={280}
-        width={500}
-        className="h-[60%] object-fill w-full"
-      />
-      {/* items-center for horizontal centring */}
-      {/* justify-center for vertical centering */}
-      <div className="flex flex-col flex-grow items-center justify-center">
-        <h2 className="text-2xl font-semibold">{event.name}</h2>
-        <p className="italic text-white/75">By {event.organizerName}</p>
-        <p className="italic text-white/50 mt-4">{event.location}</p>
-      </div>
-      {/* position date at top left corner of the card */}
-      <section className="absolute flex flex-col justify-center items-center left-[12px] top-[12px] h-[45px] w-[45px] bg-black/30 rounded-md">
-        <p className="text-xl font-bold -mb-[5px]">{dayOfMonth}</p>
-        <p className="text-xs uppercase text-accent">{month}</p>
+    <Link
+      className="flex-1 basis-80 h-[380px] max-w-[500px]"
+      href={`/event/${event.slug}`}
+    >
+      <section className="flex flex-col relative w-full h-full bg-white/[3%] rounded-xl overflow-hidden transition hover:scale-105 active:scale-[1.02] cursor-pointer">
+        {/* height and width used to prevent layout shift */}
+        <Image
+          src={event.imageUrl}
+          alt={event.name}
+          height={280}
+          width={500}
+          className="h-[60%] object-fill w-full"
+        />
+        {/* items-center for horizontal centring */}
+        {/* justify-center for vertical centering */}
+        <div className="flex flex-col flex-1 items-center justify-center">
+          <h2 className="text-2xl font-semibold">{event.name}</h2>
+          <p className="italic text-white/75">By {event.organizerName}</p>
+          <p className="italic text-white/50 mt-4">{event.location}</p>
+        </div>
+        {/* position date at top left corner of the card */}
+        <section className="absolute flex flex-col justify-center items-center left-[12px] top-[12px] h-[45px] w-[45px] bg-black/30 rounded-md">
+          <p className="text-xl font-bold -mb-[5px]">{dayOfMonth}</p>
+          <p className="text-xs uppercase text-accent">{month}</p>
+        </section>
       </section>
-    </section>
+    </Link>
   );
 }
