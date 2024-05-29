ea6a31b7e019eec6992f3057d71a9a30f20aa9bc
Victor Malchikov <victor.malchikov@netwitness.com>
2024-05-29 11:44:47 -0700

    address padding on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <main className="flex flex-col items-center px-3 pt-36">
+    <main className="flex flex-col items-center px-10 pt-36">
       <H1>Find events around you</H1>
       <p className="mb-12 mt-7 text-2xl lg:text-3xl opacity-75">
         Browse more than{" "}
