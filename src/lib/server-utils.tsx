772beda5952f3d17ce5b3db11e2f31e9c6f24470
Victor Malchikov <victor.malchikov@netwitness.com>
2024-05-30 15:52:20 -0700

    set isr time to 5 min

--- src/lib/server-utils.tsx
+++ src/lib/server-utils.tsx
@@ -9,6 +9,9 @@ import { EventoResponse } from "./types";
 // fetch API by default caches results in "data cache" so network requests are optimized and not repeated (can be opted-out of)
 // we lose this when we use the Prisma ORM to fetch data; by default Prisma does not cache results
 // we can use the unstable_cache function to cache the results of the Prisma query or any other ORM
+// docs: https://nextjs.org/docs/app/api-reference/functions/unstable_cache
+
+const MAX_CACHE_TIME_SECONDS = 300; // 5 minutes
 
 // reduces number of hits on the database
 export const getEvents = unstable_cache(
@@ -39,16 +42,22 @@ export const getEvents = unstable_cache(
       totalCount,
       events,
     };
-  }
+  },
+  ["events"],
+  { revalidate: MAX_CACHE_TIME_SECONDS }
 );
 
-export const getEvent = unstable_cache(async (slug: string) => {
-  const event: EventoEvent | null = await prisma.eventoEvent.findUnique({
-    where: { slug },
-  });
+export const getEvent = unstable_cache(
+  async (slug: string) => {
+    const event: EventoEvent | null = await prisma.eventoEvent.findUnique({
+      where: { slug },
+    });
 
-  if (!event) {
-    return notFound();
-  }
-  return event;
-});
+    if (!event) {
+      return notFound();
+    }
+    return event;
+  },
+  ["event"],
+  { revalidate: MAX_CACHE_TIME_SECONDS }
+);
